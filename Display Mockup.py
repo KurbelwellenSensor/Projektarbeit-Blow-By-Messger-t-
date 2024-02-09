6482b4a501d39403d8866ac440b30600c8e77d92
KurbelwellenSensor <156920073+KurbelwellenSensor@users.noreply.github.com>
2024-02-09 13:34:00 +0100

    Add files via upload

---
+++ Display Mockup.py
@@ -0,0 +1,9 @@
+from machine import Pin, I2C
+
+class Knopf:
+    """
+    Controller Class for the Sensor Display
+    """
+    def __init__(self, inputPin: int) -> None:
+        
+        pass
---
+++ Knopf Mockup.py
@@ -0,0 +1,9 @@
+from machine import Pin
+
+class Knopf:
+    """
+    Controller Class for the Buttons an their functions in the Sensor
+    """
+    def __init__(self, inputPin: int) -> None:
+        
+        pass
