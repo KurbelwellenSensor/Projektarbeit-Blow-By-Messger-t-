9e64fae7c58a31e5b4d7d352a0fcebc06cb70a0d
KurbelwellenSensor <156920073+KurbelwellenSensor@users.noreply.github.com>
2024-02-12 16:55:55 +0100

    Delete Knopf Mockup.py

--- Knopf Mockup.py
+++
@@ -1,9 +0,0 @@
-from machine import Pin
-
-class Knopf:
-    """
-    Controller Class for the Buttons an their functions in the Sensor
-    """
-    def __init__(self, inputPin: int) -> None:
-        
-        pass
